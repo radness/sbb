c892991853be2ed836c68485aa38ff4f63b640bb
radness <yh.shin@miracom.co.kr>
2023-03-07 10:43:48 +0900

    스프링 시큐리리 적용

--- src/main/java/com/example/sbb/SecurityConfig.java
+++ src/main/java/com/example/sbb/SecurityConfig.java
@@ -2,6 +2,8 @@ package com.example.sbb;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -15,9 +17,12 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 @EnableWebSecurity
 @EnableMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
+    // 스프링 시큐리티는 기본적으로 인증되지 않은 사용자는 서비스를 사용할 수 없게끔 되어 있다.
+    // 시큐리티 설정을 통해 sbb는 로그인 없이도 게시물을 조회할 수 있어야 한다.
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests().requestMatchers(
+                // 로그인하지 않더라도 모든 페이지 접근
                 new AntPathRequestMatcher("/**")).permitAll()
                 .and()
                 .csrf().ignoringRequestMatchers(
@@ -40,9 +45,17 @@ public class SecurityConfig {
         return http.build();
     }
 
+    // PasswordEncoder는 BCryptPasswordEncoder의 인터페이스.
+    // Bean으로 등록하지 않으면 BCryptPasswordEncoder를 구현한 모든 프로그램을 수정해야한다.
     @Bean
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
+    // AuthenticationManager는 스프링 시큐리티의 인증을 담당.
+    // AuthenticationManager Bean 생성시 스프링의 내부 동작으로 인해 UserSecurityService와 PasswordEncoder가 자동으로 설정됨.
+    @Bean
+    AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
 }
